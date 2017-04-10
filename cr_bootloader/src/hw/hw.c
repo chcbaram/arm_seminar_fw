586ec4ab5bccf6158fea709282e1f88bbfff0436
chcbaram <chcbaram@paran.com>
2017-04-11 01:10:33 +0900

    부트로더 추가

---
+++ cr_bootloader/src/hw/hw.c
@@ -0,0 +1,24 @@
+/*
+ *  hw.c
+ *
+ *  Created on: 2016. 7. 13.
+ *      Author: Baram
+ */
+
+#include "hw.h"
+
+
+
+
+
+
+void hwInit(void)
+{
+  usbInit();
+
+  ledInit();
+  microsInit();
+  vcpInit();
+}
+
+
---
+++ cr_bootloader/src/main.c
@@ -0,0 +1,52 @@
+/*
+ * main.c
+ *
+ *  Created on: 2017. 3. 19.
+ *      Author: baram
+ */
+#include "main.h"
+
+
+
+void mainInit(void);
+
+
+
+int main(void)
+{
+  uint32_t t_time;
+  uint32_t t_time_us;
+  uint32_t cnt = 0.;
+
+  mainInit();
+
+
+  t_time    = millis();
+  t_time_us = micros();
+  while(1)
+  {
+    if (millis()-t_time >= 100)
+    {
+      vcpPrintf("test %d, %d\r\n", cnt, micros()-t_time_us);
+      cnt++;
+      t_time    = millis();
+      t_time_us = micros();
+
+      ledToggle(cnt%4);
+    }
+
+    if (vcpAvailable())
+    {
+      vcpPrintf("Received : %c\r\n", vcpRead());
+    }
+  }
+
+  return 0;
+}
+
+void mainInit(void)
+{
+  bspInit();
+  hwInit();
+  apInit();
+}
